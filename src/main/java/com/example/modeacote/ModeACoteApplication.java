84d693b6f3a0c16b8fee1636febbbd483fddfe08
Sylvia B <sylwiab@gmail.com>
2023-09-11 09:51:06 +0200

    Démarrage projet Mode à Côté.

---
+++ src/main/java/com/example/modeacote/ModeACoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.modeacote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeACoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModeACoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/modeacote/ModeACoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modeacote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeACoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

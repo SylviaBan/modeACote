ec1c967d3fe2164a64e155f271194bb256b7b5fe
Sylvia B <sylwiab@gmail.com>
2023-09-11 15:33:14 +0200

    Mise en place des views : home & list.

--- src/main/java/com/example/modeacote/controller/ProductController.java
+++ src/main/java/com/example/modeacote/controller/ProductController.java
@@ -7,26 +7,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @Controller
-@RequestMapping("/products")
+@RequestMapping("/")
 public class ProductController {
     private ProductService prodService;
     public ProductController(ProductService prodService) {
         this.prodService = prodService;
     }
 
+    @GetMapping("/home")
+    public String accueil() {
+        return "home"; // Spring ajoutera automatiquement le préfixe et l'extension pour trouver "templates/home.html"
+    }
+
     // CRUD
     @GetMapping("/list")
     public String listProducts(Model model) {
-        List<Product> tutorials = prodService.findAllProd();
-
-        model.addAttribute("title","Liste des tutoriels");
-        model.addAttribute("tutorialList", tutorials);
-
-        return "home";
+        List<Product> products = prodService.findAllProd();
+        model.addAttribute("products", products);
+        return "list";
     }
 
 
@@ -34,7 +35,7 @@ public class ProductController {
     public String addProduct(@ModelAttribute Product product) {
         // Ajoutez le produit à la base de données
         prodService.addProd(product);
-        return "redirect:/products/"; // Redirige vers la liste des produits
+        return "list";
     }
 
 
@@ -42,14 +43,12 @@ public class ProductController {
     public String updateProduct(@PathVariable Long id, @ModelAttribute Product product) {
         // Mettez à jour le produit dans la base de données
         prodService.updateProd(id, product);
-        return "redirect:/products/"; // Redirige vers la liste des produits
-    }
+        return "list";    }
 
     // Supprimer un produit
     @GetMapping("/delete/{id}")
     public String deleteProduct(@PathVariable Long id) {
-        // Supprimez le produit de la base de données
         prodService.deleteProdById(id);
-        return "redirect:/products/"; // Redirige vers la liste des produits
+        return "list";
     }
 }
